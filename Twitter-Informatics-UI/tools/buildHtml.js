572238b2e17f09a46326e721bc64966f3ab7c0a6
DivyenduDutta <connect2divyendu@gmail.com>
2018-09-16 17:42:15 +0530

    added code for production build in react

---
+++ Twitter-Informatics-UI/tools/buildHtml.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import cheerio from 'cheerio';
+
+/*eslint-disable no-console */
+
+fs.readFile('src/index.html', 'utf-8', (err, markup) =>{
+    if(err){
+        return console.log(err);
+    }
+
+    const $ = cheerio.load(markup);
+
+    $('head').prepend('<link rel="stylesheet" href="styles.css">');
+
+    fs.writeFile('dist/index.html', $.html(), 'utf-8', function(err){
+        if(err){
+            return console.log(err);
+        }
+        console.log('index.html written');
+    });
+});
--- src/main/java/com/divyendu/comtroller/ProfileController.java
+++ src/main/java/com/divyendu/comtroller/ProfileController.java
@@ -2,6 +2,7 @@ package com.divyendu.comtroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,8 @@ public class ProfileController {
 		this.profileManager = profileManager;
 	}
 	
+	//Enabling CORS so that our react app can send requests to out rest service	
+	@CrossOrigin(origins = "http://localhost:3000")	
 	@RequestMapping(value = "/profile")
 	public TwitterProfile getScreenName() {
 		return profileManager.retrieveProfileData();
