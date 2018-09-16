b22d407cd9da02b9e4c6188a5544e21a98570cb9
DivyenduDutta <connect2divyendu@gmail.com>
2018-09-16 21:26:08 +0530

    Pom committed1

--- Twitter-Informatics-UI/src/routes.js
+++ Twitter-Informatics-UI/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route,IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-
+  
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} /> 
