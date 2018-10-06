f88212abed6766e0b81cb7d38deca317c4ebafc8
DivyenduDutta <connect2divyendu@gmail.com>
2018-10-06 23:25:47 +0530

    added dependancies for actuator and devtools in pom.xml

---
+++ src/main/java/com/divyendu/comtroller/ProfileController1.java
@@ -0,0 +1,29 @@
+package com.divyendu.comtroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.divyendu.impl.ProfileManagerImpl;
+
+@RestController
+public class ProfileController1 {
+	
+	private ProfileManagerImpl profileManager;
+
+	@Autowired
+	public ProfileController1(ProfileManagerImpl profileManager) {
+		this.profileManager = profileManager;
+	}
+	
+	//Enabling CORS so that our react app can send requests to out rest service	
+	@CrossOrigin(origins = "http://localhost:3000")	
+	@RequestMapping(value = "/profile")
+	public TwitterProfile getScreenName() {
+		return profileManager.retrieveProfileData();
+	}
+	
+
+}
